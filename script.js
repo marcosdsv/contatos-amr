0e9994bbb8c36ca5624dfc7716881b6d9d8f4bec
Eduardo Quirino <edu.3532quirino@gmail.com>
2023-08-03 16:23:41 -0300

    falta botão modo escuro e auto descrição

---
+++ script.js
@@ -0,0 +1,15 @@
+// Código JavaScript personalizado
+// Adicione eventos de hover para mudar a cor dos ícones de redes sociais no rodapé
+document.addEventListener('DOMContentLoaded', function () {
+  const socialIcons = document.querySelectorAll('.social-icons a');
+
+  socialIcons.forEach(icon => {
+    icon.addEventListener('mouseover', function () {
+      this.classList.add('text-danger'); // Adiciona a classe 'text-danger' para mudar a cor do ícone
+    });
+
+    icon.addEventListener('mouseout', function () {
+      this.classList.remove('text-danger'); // Remove a classe 'text-danger' para voltar à cor original do ícone
+    });
+  });
+});
